e925f56b37b3cdbd6f2020e18aeab5362f568d58
郭兵 <m13571829170@163.com>
2019-01-15 11:54:10 +0800

    Spring Boot运行原理

---
+++ src/main/java/com/talnet/SpringBoot07Application.java
@@ -0,0 +1,14 @@
+package com.talnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot07Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot07Application.class, args);
+    }
+
+}
+
